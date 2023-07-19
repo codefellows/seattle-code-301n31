784c6742c5792b60bb7e79dc4b869c65713ed0bb
Roger Reyes <Rogerrmr14@gmail.com>
2023-07-19 17:57:19 +0900

    Added class 04 lecture demo

---
+++ class-04/demo/lecture-demo/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Gallery of Students</h1>
+      </>
+    )
+  }
+}
+
+export default Header;
